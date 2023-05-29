2d17c3485cf4db80311d17ea75d43514eb8a5562
Guduru Kiran Sai <119746361+kiransais03@users.noreply.github.com>
2023-05-29 18:18:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 // complete the given function
 
 function palindrome(str){
+	let lowerecaseString=str.toLowerCase();
+	
+	let i=0;
 
-}
+	 let j=lowerecaseString.length-1;
+	 while(i<j)
+		 {
+			 if(lowerecaseString.charAt(i)!==lowerecaseString.charAt(j))
+			 {
+				 return false;
+			 }
+			 i++;
+			 j--;
+		 }
+	 return true;
+ }
 module.exports = palindrome
